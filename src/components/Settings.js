588f05f114966e90ec072cd20d58c0e09f79bd48
Andrew Maddocks <andrewmadd@icloud.com>
2019-12-23 12:28:47 -0700

    set up react app sass and login page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,87 +2,224 @@ import React, { useState } from "react";
 import { withFormik, Form, Field } from "formik";
 import axiosWithAuth from "../utils/axiosWithAuth";
 import styled from "styled-components";
+// import TableUserCard from "./TableUserCard";
+import Dropdown from "./Dropdown";
+import { Link } from "react-router-dom";
 
 const Title = styled.h1`
   color: #177c84;
   font-size: 44px;
 `;
+
 const Div1 = styled.div`
   // box-shadow: 0 0.5rem 0.5rem rgba(0, 0, 0, 0.2);
   min-width: 35%;
   padding: 3%;
   margin: 0 auto;
 `;
+
 const Div2 = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
-
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Div3 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 240px;
+  margin-top: 20px;
+`;
+const Div4 = styled.div`
+  display: flex;
+  color: #177c84;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+const Div5 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 const FieldInfo = styled(Field)`
   border-radius: 20px;
   border: 1px solid gray;
   width: 200px;
-  padding: 10px;
+  height: 10px;
+  padding: 15px;
+  margin-top: 5px;
+`;
+const DateAndTotal = styled(Field)`
+  border-radius: 20px;
+  border: 1px solid gray;
+  width: 125px;
+  height: 10px;
+  padding: 15px;
+  margin: 5px;
+`;
+const Addbutton = styled(Field)`
+  background: none;
+  border: none;
+  margin: 5px;
+  padding: none;
+  &:active {
+    outline: none;
+    border: none;
+  }
 `;
 const Fieldbutton = styled(Field)`
   border-radius: 20px;
   border: 1px solid #bdc3c7;
-  width: 230px;
+  width: 200px;
   padding: 10px;
-  background: #7eb2b7;
+  background: #177c84;
   color: white;
   font-size: 15px;
-  margin-bottom: 20px;
+  margin-top: 20px;
 `;
 
 const Label3 = styled.label`
-  margin-right: 170px;
-  margin-bottom: 8px;
+  margin-right: 150px;
+  color: #177c84;
+`;
+const FriendsLabel = styled.label`
+  margin-top: 30px;
+  margin-right: 150px;
+  color: #177c84;
 `;
 
 const Settings = ({ values }) => {
-  const [updateUser, setUpdateUser] = useState({
-    firstName: " "
-  });
-  const [deleteUser, setDeleteUser] = useState([[]]);
+  const [hiddenUsers, setHiddenUsers] = useState(new Array(16).fill(true));
 
+  const toggleHide = () => {
+    const nextIndex = hiddenUsers.findIndex(displayed => {
+      return displayed === true;
+    });
+    const nextArray = hiddenUsers;
+    nextArray[nextIndex] = !nextArray[nextIndex];
+    setHiddenUsers(nextArray);
+  };
   return (
     <Div1>
-      <Title>Update User</Title>
+      <Dropdown />
+      <Title>Split-The-Bill</Title>
       <Form>
         <Div2>
-          <Label3>firstName</Label3>
-          <FieldInfo type="text" first name="name" />
+          <Label3>Your Name </Label3>
+          <FieldInfo
+            type="text"
+            name="owner"
+            placeholder="please add your name.."
+          />
         </Div2>
-        <Fieldbutton className="field" as="button" type="submit" name="submit">
-          Update
+        <Div2>
+          <FriendsLabel>Bill Name </FriendsLabel>
+          <FieldInfo
+            type="text"
+            name="billName"
+            placeholder="what's this for?"
+          />
+        </Div2>
+        <Div>
+          <Div3>
+            <Div4>
+              <label>Date</label>
+              <DateAndTotal type="date" name="date" />
+            </Div4>
+            <Div4>
+              <label> Total </label>
+              <DateAndTotal type="text" name="total" placeholder="$" />
+            </Div4>
+          </Div3>
+        </Div>
+        <Div2>
+          <FriendsLabel>Friends </FriendsLabel>
+          <FieldInfo
+            type="text"
+            name="friends"
+            placeholder="add friends separated by commas..."
+          />
+        </Div2>
+        {/* <TableUserCard />
+        {hiddenUsers.map((user, index) => (
+          <TableUserCard hidden={hiddenUsers[index]} />
+        ))} 
+
+        <div>
+          <Addbutton
+            type="submit"
+            name="button"
+            as="button"
+            o  nClick={toggleHide}
+          >
+            <i
+              class="material-icons"
+              style={{
+                color: "#177c84",
+                fontSize: "45px",
+                marginLeft: "180px"
+              }}
+            >
+              add
+            </i>
+          </Addbutton>
+            </div> */}
+        <Div5>
+          <FriendsLabel>Notes</FriendsLabel>
+          <FieldInfo
+            type="text"
+            name="notes"
+            placeholder="Leave a comment..."
+          />
+        </Div5>
+        ​
+        <Fieldbutton as="button" type="submit" name="submit">
+          Submit Table
         </Fieldbutton>
       </Form>
     </Div1>
   );
 };
-const FormikSettings = withFormik({
-  mapPropsToValues({ firstName }) {
+const FormikLogIn = withFormik({
+  mapPropsToValues({ billName, total, date, friends, notes, owner }) {
     return {
-      firstName: firstName || ""
+      billName: billName || "",
+      total: total || "",
+      date: date || "",
+      friends: friends || "",
+      notes: notes || "",
+      owner: owner || ""
     };
   },
-  handleSubmit(values, formikBag) {
-    console.log("VALUES", values);
-    axios
-      .put("https://bw-split-the-bill.herokuapp.com/api/auth/login", values)
-      .then(res => {
-        localStorage.setItem("token", res.data.token);
-        // formikBag.props.setUser(res.data);
 
-        console.log(res);
-        console.log("formikBag Props", formikBag.props);
+  handleSubmit(values, formikBag) {
+    values.friends = values.friends.split(",").map(friend => {
+      return {
+        username: friend.trim(),
+        paid: false
+      };
+    });
+
+    values.total = parseInt(values.total);
+    console.log("values", values);
+    // history.push("/dashboard")
+    axiosWithAuth()
+      .post("/api/bills", values)
+      .then(res => {
         formikBag.props.history.push("/dashboard");
+        console.log(res);
+        //redirect user to dashboard with history.push
       })
       .catch(err => console.log(err.response));
   }
 })(Settings);
 
-export default FormikSettings;
+export default FormikLogIn;
